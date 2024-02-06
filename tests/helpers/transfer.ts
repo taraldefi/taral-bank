073629f10c6cb0dc1f5bbf3151ac65ece1416429
Zishan <amanzishan.az@gmail.com>
2024-02-06 15:01:09 +0530

    fix(contracts): fixed decimals for contracts and updated tests for amount intake

--- tests/helpers/transfer.ts
+++ tests/helpers/transfer.ts
@@ -1,30 +1,33 @@
-import { expect } from "vitest";
-import { MICRO_MULTIPLIER } from "./currency";
+import { expect } from 'vitest';
+import { MICRO_MULTIPLIER } from './currency';
 
 const accounts = simnet.getAccounts();
-const DEPLOYER = accounts.get("deployer")!;
+const DEPLOYER = accounts.get('deployer')!;
 
 /*
-     * Helper function to assert that a transfer event is a USDa transfer 
-    */
-export function expectSUSDTTransfer(transferEvent: any, sender: string, recipient: string, amount: any) {
+ * Helper function to assert that a transfer event is a USDa transfer
+ */
+export function expectSUSDTTransfer(
+	transferEvent: any,
+	sender: string,
+	recipient: string,
+	amount: any,
+) {
+	let senderAddress = sender;
+	if (sender === DEPLOYER) {
+		senderAddress = `${sender}.taral-bank`;
+	}
 
-    let senderAddress = sender;
-    if (sender === DEPLOYER) {
-        senderAddress = `${sender}.taral-bank`;
-    }
+	let recipientAddress = recipient;
+	if (recipient === DEPLOYER) {
+		recipientAddress = `${recipient}.taral-bank`;
+	}
 
-    let recipientAddress = recipient;
-    if (recipient === DEPLOYER) {
-        recipientAddress = `${recipient}.taral-bank`;
-    }
+	expect(transferEvent.asset_identifier).toStrictEqual(
+		`${DEPLOYER}.token-susdt::bridged-usdt`,
+	);
 
-
-    expect(transferEvent.asset_identifier).toStrictEqual(
-        `${DEPLOYER}.token-susdt::bridged-usdt`,
-    );
-
-    expect(transferEvent.sender).toStrictEqual(senderAddress);
-    expect(transferEvent.recipient).toStrictEqual(recipientAddress);
-    expect(transferEvent.amount).toStrictEqual(`${amount * MICRO_MULTIPLIER}`);
+	expect(transferEvent.sender).toStrictEqual(senderAddress);
+	expect(transferEvent.recipient).toStrictEqual(recipientAddress);
+	expect(transferEvent.amount).toStrictEqual(`${amount}`);
 }
